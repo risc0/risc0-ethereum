220255258a18832d3d46d4d8cf38cdff2899afe1
Victor Graf <victor@risczero.com>
2024-04-22 17:16:21 -0700

    rename PausableVerifier to RiscZeroVerifierEmergencyStop and add unpriviledged estop

--- contracts/src/RiscZeroVerifierEmergencyStop.sol
+++ contracts/src/RiscZeroVerifierEmergencyStop.sol
@@ -22,7 +22,7 @@ import {Pausable} from "openzeppelin/contracts/utils/Pausable.sol";
 import {IRiscZeroVerifier, Receipt} from "./IRiscZeroVerifier.sol";
 
 /// @notice Wrapper for an IRiscZeroVerifier contracts with emergency stop function.
-contract PausableVerifier is IRiscZeroVerifier, Ownable, Pausable {
+contract RiscZeroVerifierEmergencyStop is IRiscZeroVerifier, Ownable, Pausable {
     IRiscZeroVerifier immutable verifier;
 
     constructor(IRiscZeroVerifier _verifier) Ownable(_msgSender()) {
@@ -30,10 +30,22 @@ contract PausableVerifier is IRiscZeroVerifier, Ownable, Pausable {
     }
 
     /// @notice Initiate an emergency stop of the wrapped verifier contract.
+    ///         This method can only be used by guardian address assigned as owner of this contract.
     ///         When stopped, all calls to the verify and verifyIntegrity functions will revert.
     ///         Once stopped, this contract can never be restarted.
-    // TODO(victor): Call this something like emergency stop instead of "pause".
-    function pause() external onlyOwner {
+    function estop() external onlyOwner {
+        _pause();
+    }
+
+    /// @notice Initiate an emergency stop of the wrapped verifier contract.
+    ///         This method can be called by anyone who can produce a verifying proof for a receipt
+    ///         claim digest of all zeroes. The existence of such a proof demonstrates a critical
+    ///         vulnerability in the proof system.
+    ///         When stopped, all calls to the verify and verifyIntegrity functions will revert.
+    ///         Once stopped, this contract can never be restarted.
+    function estop(Receipt calldata receipt) external {
+        require(receipt.claimDigest == bytes32(0), "receipt claim digest is non-zero");
+        require(verifyIntegrity(receipt), "receipt verification failed");
         _pause();
     }
 
@@ -49,7 +61,7 @@ contract PausableVerifier is IRiscZeroVerifier, Ownable, Pausable {
     }
 
     /// @inheritdoc IRiscZeroVerifier
-    function verifyIntegrity(Receipt memory receipt) external view whenNotPaused returns (bool) {
+    function verifyIntegrity(Receipt calldata receipt) public view whenNotPaused returns (bool) {
         // Forward the call on to the wrapped contract.
         return verifier.verifyIntegrity(receipt);
     }
