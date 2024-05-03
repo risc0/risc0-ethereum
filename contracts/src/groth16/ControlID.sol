07b74698c7402de6b23bbc1d0d0fd9f2c15087f5
Victor Graf <victor@subgraf.dev>
2024-05-03 20:55:10 +0000

    run cargo xtask bootstrap-groth16 from 69d953ce56a7209ed8e6a6a3fff3cf209e0779d3

--- contracts/src/groth16/ControlID.sol
+++ contracts/src/groth16/ControlID.sol
@@ -20,7 +20,7 @@
 pragma solidity ^0.8.9;
 
 library ControlID {
-    uint256 public constant CONTROL_ID_0 = 0x51a3d73938c3681118ba0a2549f7c188;
-    uint256 public constant CONTROL_ID_1 = 0x44f39e6e6cef91de6d743e7f5b7a1e67;
+    uint256 public constant CONTROL_ID_0 = 0x0bebe2a56ec6879d146d8bf7050e3175;
+    uint256 public constant CONTROL_ID_1 = 0x135186fd4c9f9c31171058f045fc5a3d;
     uint256 public constant BN254_CONTROL_ID = 0x10ff834dbef62ccbba201ecd26a772e3036a075aacbaf47200679a11dcdcf10d;
 }
