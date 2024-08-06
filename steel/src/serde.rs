3b6b162540323aef44110646189358940ec3675e
Wolfgang Welz <welzwo@gmail.com>
2024-08-06 11:23:21 +0200

    add comment

--- steel/src/serde.rs
+++ steel/src/serde.rs
@@ -103,6 +103,8 @@ impl<'de, H: Encodable + Decodable> Deserialize<'de> for RlpHeader<H> {
         } else {
             deserializer.deserialize_byte_buf(BytesVisitor)?
         };
+        // the RLP-encoding is not malleable, as long as we make sure that there are no additional
+        // bytes after the RLP-encoded data
         let inner = alloy_rlp::decode_exact(&rlp).map_err(de::Error::custom)?;
 
         Ok(RlpHeader {
