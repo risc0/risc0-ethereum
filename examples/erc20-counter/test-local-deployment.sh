2dc02b86820733f78c93870694818512c966879f
Wolfgang Welz <welzwo@gmail.com>
2024-09-06 21:18:56 +0200

    use random Anvil port

--- examples/erc20-counter/test-local-deployment.sh
+++ examples/erc20-counter/test-local-deployment.sh
@@ -15,10 +15,13 @@ cleanup() {
 # This ensures cleanup is performed on script exit or error
 trap cleanup EXIT ERR
 
+# Get a random port number between 49152 and 65535
+ANVIL_PORT=$(( RANDOM % 16384 + 49152 ))
+
 # Start Anvil and capture its output temporarily
-anvil > anvil_logs.txt 2>&1 &
+anvil -p $ANVIL_PORT > anvil_logs.txt 2>&1 &
 ANVIL_PID=$!
-echo "Anvil started with PID $ANVIL_PID"
+echo "Anvil started with on port $ANVIL_PORT with PID $ANVIL_PID"
 
 # Wait a few seconds to ensure Anvil has started and output private keys
 sleep 5
@@ -32,7 +35,7 @@ cargo build
 
 # Deploy the Counter contract
 echo "Deploying the Counter contract..."
-forge script --rpc-url http://localhost:8545 --broadcast DeployCounter
+forge script --rpc-url http://localhost:$ANVIL_PORT --broadcast DeployCounter
 
 # Extract the Toyken address
 export TOYKEN_ADDRESS=$(jq -re '.transactions[] | select(.contractName == "ERC20FixedSupply") | .contractAddress' ./broadcast/DeployCounter.s.sol/31337/run-latest.json)
@@ -45,14 +48,14 @@ echo "Counter Address: $COUNTER_ADDRESS"
 # Publish a new state
 echo "Publishing a new state..."
 cargo run --bin publisher -- \
-    --eth-rpc-url=http://localhost:8545 \
+    --eth-rpc-url=http://localhost:$ANVIL_PORT \
     --counter=${COUNTER_ADDRESS:?} \
     --token-contract=${TOYKEN_ADDRESS:?} \
     --account=${TOKEN_OWNER:?}
 
 # Attempt to verify counter value as part of the script logic
 echo "Verifying state..."
-COUNTER_VALUE=$(cast call --rpc-url http://localhost:8545 ${COUNTER_ADDRESS:?} 'get()(uint256)')
+COUNTER_VALUE=$(cast call --rpc-url http://localhost:$ANVIL_PORT ${COUNTER_ADDRESS:?} 'get()(uint256)')
 if [ "$COUNTER_VALUE" != "1" ]; then
     echo "Counter value is not 1 as expected, but $COUNTER_VALUE."
     exit 1
