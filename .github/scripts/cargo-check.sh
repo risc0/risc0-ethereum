46fd2f0dcb367b6bf842dc6fe34d853125625aa2
Wolfgang Welz <welzwo@gmail.com>
2024-09-10 13:23:26 +0200

    check all examples in CI

---
+++ .github/scripts/cargo-check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+fmt_sort_clippy(){
+  while read path; do
+    printf "Project: %s\n" "$path"
+    cargo fmt --all --check --manifest-path "$path"
+    (cd "${path%/*}"; cargo sort --workspace --check)
+    RISC0_SKIP_BUILD=true cargo clippy --workspace --all-targets --all-features --manifest-path "$path" -- -D warnings
+  done
+}
+
+find . -maxdepth 2 -mindepth 2 -name 'Cargo.toml' | fmt_sort_clippy
