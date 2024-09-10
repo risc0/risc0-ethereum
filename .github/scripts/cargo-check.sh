0eafaffb9d312caa22811a5b59dc8289c39ef0fa
Wolfgang Welz <welzwo@gmail.com>
2024-09-10 16:17:38 +0200

    separate jobs

--- .github/scripts/cargo-check.sh
+++ .github/scripts/cargo-check.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 
-fmt_sort_clippy(){
+fmt_sort(){
   while read path; do
     printf "Project: %s\n" "$path"
     cargo fmt --all --check --manifest-path "$path"
     (cd "${path%/*}"; cargo sort --workspace --check)
-    RISC0_SKIP_BUILD=true cargo clippy --workspace --all-targets --all-features --manifest-path "$path" -- -D warnings
   done
 }
 
-grep -rl --include "Cargo.toml" '\[workspace\]' | sort -u | fmt_sort_clippy
+grep -rl --include "Cargo.toml" '\[workspace\]' | sort -u | fmt_sort
