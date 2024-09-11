8df05ab402d533f00f98036e884b413da7b6a49e
Wolfgang Welz <welzwo@gmail.com>
2024-09-12 00:41:48 +0200

    WEB3-112: chore: Steel e2e tests (#228)
    This PR turns the `erc20-counter` example into an end-to-end test. It
    uses a local Ethereum devnet including an execution and consensus client
    based on the
    [ethpandaops/ethereum-package](https://github.com/ethpandaops/ethereum-package)
    Kurtosis package.
    In the CI, all contracts are deployed on the devnet before a full proof
    is generated and validated with a block and a beacon commitment.
    
    closes #203
    closes WEB3-90
    
    ---------
    
    Co-authored-by: Victor Graf <victor@risczero.com>

---
+++ examples/erc20-counter/e2e-test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Test the integration of the application contract and publisher, against a running EVM chain.
+set -e -o pipefail
+
+export TOKEN_OWNER=${ETH_WALLET_ADDRESS:?}
+
+# Determine the chain ID
+CHAIN_ID=$(cast rpc --rpc-url ${ETH_RPC_URL:?} eth_chainId | jq -re)
+CHAIN_ID=$((CHAIN_ID))
+
+# Deploy the Counter contract
+echo "Deploying the Counter contract..."
+forge script --rpc-url ${ETH_RPC_URL:?} --private-key ${ETH_WALLET_PRIVATE_KEY:?} --broadcast DeployCounter
+
+# Extract the Toyken address
+TOYKEN_ADDRESS=$(jq -re '.transactions[] | select(.contractName == "ERC20FixedSupply") | .contractAddress' ./broadcast/DeployCounter.s.sol/$CHAIN_ID/run-latest.json)
+echo "ERC20 Toyken Address: $TOYKEN_ADDRESS"
+
+# Extract the Counter contract address
+COUNTER_ADDRESS=$(jq -re '.transactions[] | select(.contractName == "Counter") | .contractAddress' ./broadcast/DeployCounter.s.sol/$CHAIN_ID/run-latest.json)
+echo "Counter Address: $COUNTER_ADDRESS"
+
+# Publish a new state
+echo "Publishing a new state..."
+RUST_LOG=info,risc0_steel=debug cargo run --bin publisher -- \
+    --eth-wallet-private-key=${ETH_WALLET_PRIVATE_KEY:?} \
+    --eth-rpc-url=${ETH_RPC_URL:?} \
+    --counter-address=${COUNTER_ADDRESS:?} \
+    --token-contract=${TOYKEN_ADDRESS:?} \
+    --account=${TOKEN_OWNER:?}
+
+# Attempt to verify counter value as part of the script logic
+echo "Verifying state..."
+COUNTER_VALUE=$(cast call --rpc-url ${ETH_RPC_URL:?} ${COUNTER_ADDRESS:?} 'get()(uint256)')
+if [ "$COUNTER_VALUE" != "1" ]; then
+    echo "Counter value is not 1 as expected, but $COUNTER_VALUE."
+    exit 1
+fi
