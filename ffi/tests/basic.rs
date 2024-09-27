cdde22eb9f8d3c562a47187cb795e59279959917
Victor Graf <victor@risczero.com>
2024-09-27 13:27:57 -0700

    add another test

--- ffi/tests/basic.rs
+++ ffi/tests/basic.rs
@@ -20,7 +20,7 @@ use risc0_ethereum_contracts::encode_seal;
 use risc0_zkvm::{FakeReceipt, InnerReceipt, Receipt, ReceiptClaim};
 
 #[test]
-fn main() {
+fn basic_usage() {
     let exe_path = env!("CARGO_BIN_EXE_risc0-forge-ffi");
     let args = ["prove", ECHO_PATH, "0xdeadbeef"];
     println!("{} {:?}", exe_path, args);
@@ -29,6 +29,40 @@ fn main() {
         // PATH is required so r0vm can be found.
         .env("PATH", std::env::var("PATH").unwrap())
         .env("RISC0_DEV_MODE", "1")
+        .args(args)
+        .output()
+        .unwrap();
+
+    println!("{:#?}", &output);
+
+    let output_bytes = hex::decode(output.stdout).unwrap();
+    let (journal, seal) = <(Bytes, Bytes)>::abi_decode(&output_bytes, true).unwrap();
+
+    assert_eq!(journal, hex::decode("deadbeef").unwrap());
+    let expected_receipt = Receipt::new(
+        InnerReceipt::Fake(FakeReceipt::new(ReceiptClaim::ok(
+            ECHO_ID,
+            journal.to_vec(),
+        ))),
+        journal.into(),
+    );
+    let expect_seal = encode_seal(&expected_receipt).unwrap();
+    assert_eq!(expect_seal, seal.to_vec());
+}
+
+// It's important that `risc0-forge-ffi` only send to stdout the output to be consumed by forge
+// with the FFI cheatcode. If any extra output is sent, ABI decoding will fail.
+#[test]
+fn basic_usage_with_rust_log() {
+    let exe_path = env!("CARGO_BIN_EXE_risc0-forge-ffi");
+    let args = ["prove", ECHO_PATH, "0xdeadbeef"];
+    println!("{} {:?}", exe_path, args);
+    let output = Command::new(exe_path)
+        .env_clear()
+        // PATH is required so r0vm can be found.
+        .env("PATH", std::env::var("PATH").unwrap())
+        .env("RISC0_DEV_MODE", "1")
+        .env("RUST_LOG", "debug")
         .args(args)
         .output()
         .unwrap();
