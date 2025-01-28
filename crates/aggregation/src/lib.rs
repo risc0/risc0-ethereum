8a0d89e49d89aa18cbb0a28d81597b3f93bf38df
capossele <angelocapossele@gmail.com>
2025-01-28 15:00:40 +0000

    cherry pick #365

--- crates/aggregation/src/lib.rs
+++ crates/aggregation/src/lib.rs
@@ -32,12 +32,12 @@ mod receipt;
 
 #[cfg(feature = "verify")]
 pub use receipt::{
-    EncodingError, RecursionVerifierParamters, SetInclusionReceipt,
-    SetInclusionReceiptVerifierParameters,
-    VerificationError,
+    decode_set_inclusion_seal, RecursionVerifierParamters, SetInclusionDecodingError,
     /* TODO(#353)
     SET_BUILDER_ELF, SET_BUILDER_ID, SET_BUILDER_PATH,
     */
+    SetInclusionEncodingError, SetInclusionReceipt, SetInclusionReceiptVerifierParameters,
+    VerificationError,
 };
 
 alloy_sol_types::sol! {
