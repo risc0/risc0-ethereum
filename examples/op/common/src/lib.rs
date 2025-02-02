9133176be95b642c463072e380a532840928141a
Wolfgang Welz <welzwo@gmail.com>
2025-02-02 15:02:27 +0100

    fix ProviderBuilder

--- examples/op/common/src/lib.rs
+++ examples/op/common/src/lib.rs
@@ -28,7 +28,7 @@ pub async fn verify_on_chain(
     let journal = receipt.journal.bytes;
 
     let provider = ProviderBuilder::new()
-        .on_anvil_with_wallet_and_config(|anvil| anvil.fork(rpc_url));
+        .on_anvil_with_wallet_and_config(|anvil| anvil.fork(rpc_url))?;
 
     alloy::sol!(
         #[sol(rpc)]
