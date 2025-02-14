196f7921dc5660f937ac7bb13de9b212d18a2828
planetBoy <140164174+Guayaba221@users.noreply.github.com>
2025-02-14 21:48:42 +0100

    chore: fix spelling issue in function / comment (#439)
    contracts/deployment-test/Deployment.t.sol
    ``confgiuration``  -  ``configuration`` ( fix typo ) 
    
    contracts/script/DeployVerifier.s.sol
    ``../src//IRiscZeroVerifier.sol`` - ``../src/IRiscZeroVerifier.sol`` (
    delete / )
    
    contracts/src/IRiscZeroSelectable.sol
    ``parameters If two``  -  ``parameters. If two`` ( add . )
    
    ---------
    
    Co-authored-by: Wolfgang Welz <welzwo@gmail.com>

--- contracts/src/IRiscZeroSelectable.sol
+++ contracts/src/IRiscZeroSelectable.sol
@@ -1,4 +1,4 @@
-// Copyright 2024 RISC Zero, Inc.
+// Copyright 2025 RISC Zero, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ pragma solidity ^0.8.9;
 /// @notice Selectable interface for RISC Zero verifier.
 interface IRiscZeroSelectable {
     /// @notice A short key attached to the seal to select the correct verifier implementation.
-    /// @dev The selector is taken from the hash of the verifier parameters If two
+    /// @dev The selector is taken from the hash of the verifier parameters. If two
     ///      receipts have different selectors (i.e. different verifier parameters), then it can
     ///      generally be assumed that they need distinct verifier implementations. This is used as
     ///      part of the RISC Zero versioning mechanism.
