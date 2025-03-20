90877d9a2e8cac579ef137296380b1a6af817871
Angelo Capossele <angelocapossele@gmail.com>
2025-03-20 21:57:13 +0000

    WEB3-384: Bootstrap groth16 parameters (#501)

--- contracts/test/RiscZeroGroth16Verifier.t.sol
+++ contracts/test/RiscZeroGroth16Verifier.t.sol
@@ -138,6 +138,6 @@ contract RiscZeroGroth16VerifierTest is Test {
     }
 
     function testSelectorIsStable() external view {
-        require(verifier.SELECTOR() == hex"ff146928");
+        require(verifier.SELECTOR() == hex"9f39696c");
     }
 }
