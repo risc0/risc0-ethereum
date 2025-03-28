efd165130dc3fd6045881f6623857205e318c90e
Willem Olding <willemolding@gmail.com>
2025-03-28 14:27:28 +1300

    limit visibility of block_hash fn

--- crates/steel/src/block.rs
+++ crates/steel/src/block.rs
@@ -117,7 +117,7 @@ impl<H: EvmBlockHeader> BlockInput<H> {
         EvmEnv::new(db, header, commit)
     }
 
-    pub fn block_hash(&self) -> B256 {
+    pub(crate) fn block_hash(&self) -> B256 {
         self.header.seal_ref_slow().seal()
     }
 }
