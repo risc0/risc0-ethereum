0dad142034fa255f1908c384fcbaaa718375995e
Victor Graf <victor@risczero.com>
2025-08-14 15:25:47 -0700

    add dummy crates to the steel dirs

---
+++ crates/op-steel/src/lib.rs
@@ -0,0 +1,1 @@
+compile_error!("risc0-op-steel has moved. Please reference https://github.com/boundless-xyz/steel");
---
+++ crates/steel/src/lib.rs
@@ -0,0 +1,1 @@
+compile_error!("risc0-steel has moved. Please reference https://github.com/boundless-xyz/steel");
