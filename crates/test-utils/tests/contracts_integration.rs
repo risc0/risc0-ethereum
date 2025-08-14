4694b636ebe652eb3627f3da93d817a53f2977f0
Victor Graf <victor@risczero.com>
2025-08-13 17:18:24 -0700

    set dev mode on macos

--- crates/test-utils/tests/contracts_integration.rs
+++ crates/test-utils/tests/contracts_integration.rs
@@ -12,22 +12,30 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use alloy::node_bindings::Anvil;
 use anyhow::Context;
-use risc0_ethereum_test_utils::{prove_echo, text_ctx};
+use risc0_ethereum_test_utils::{prove_echo, test_ctx_with};
 use risc0_ethereum_test_utils_guests::ECHO_ID;
-use risc0_zkvm::sha;
 use risc0_zkvm::sha::Sha256;
 use risc0_zkvm::ProverOpts;
+use risc0_zkvm::{sha, VerifierContext};
+use tokio::sync::Mutex;
 
 #[tokio::test]
 async fn basic() -> anyhow::Result<()> {
-    // TODO(victor): This message needs to be be a multiple of 4 bytes in length because there is
-    // some kind of bug in the `env.stdin().read_to_end()` implementation that causes the end to be
-    // padded with zeroes. I am also working on figuring out what is going on.
     const MSG: &str = "hello risc0!";
 
-    let ctx = text_ctx().await.context("failed to setup test context")?;
-    let receipt = prove_echo(MSG, ProverOpts::groth16()).await?.receipt;
+    let anvil = Anvil::new().spawn();
+    // On MacOS, we cannot produce a Groth16 proof (risc0#1749), so we always set dev mode.
+    let verifier_ctx = if cfg!(target_os = "macos") {
+        VerifierContext::default().with_dev_mode(true)
+    } else {
+        VerifierContext::default()
+    };
+    let prover_opts = ProverOpts::groth16().with_dev_mode(verifier_ctx.dev_mode());
+
+    let ctx = test_ctx_with(Mutex::new(anvil).into(), 0, verifier_ctx).await?;
+    let receipt = prove_echo(MSG, prover_opts).await?.receipt;
     let encoded_seal = risc0_ethereum_contracts::encode_seal(&receipt)?;
     let journal_digest = sha::Impl::hash_bytes(MSG.as_ref());
     ctx.verifier
